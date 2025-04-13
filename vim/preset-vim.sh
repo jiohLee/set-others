d7ae8b12434c603e6bf736a0d7a955015edf70b9
jiohLee <jiosiro@gmail.com>
2025-04-14 00:23:26 +0900

    reorder directories & modify vim settings

--- vim/preset-vim.sh
+++ vim/preset-vim.sh
@@ -1,43 +1,56 @@
 # vim configuration
-git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-echo -e "set nocompatible
+cat << 'EOF' > ~/.vimrc
+set nocompatible
 filetype off
 set rtp+=~/.vim/bundle/Vundle.vim
 call vundle#begin()
 
+" vundle plugin manager
 Plugin 'VundleVim/Vundle.vim'
+
+" editor environment
 Plugin 'scrooloose/nerdtree'
 Plugin 'vim-airline/vim-airline'
-
 Plugin 'honza/vim-snippets'
 Plugin 'SirVer/ultisnips'
 
+" colorscheme
 Plugin 'sonph/onehalf', {'rtp': 'vim/'}
 
 call vundle#end()
-filetype plugin indent on"> ~/.vimrc
+filetype plugin indent on
+EOF
 
 vim +PluginInstall +qall
 
-echo -e "
+cat << 'EOF' >> ~/.vimrc
+
 set nu
 set ts=4
 set sw=4
 set ls=2
 set cindent
 
+" set colorscheme
 colorscheme onehalfdark
 let g:airline_theme='onehalfdark'
 highlight Normal ctermfg = white ctermbg = NONE
 highligh LineNr ctermbg = NONE
 
+" set NERDTree
 nmap <C-b> :NERDTreeToggle<CR>
 nnoremap <Esc>[1;5C :tabnext<CR>
 nnoremap <Esc>[1;5D :tabprevious<CR>
 
+" set UltiSnips
 let g:UltiSnipsExpandTrigger='<C-a>'
 let g:UltiSnipsJumpForwardTrigger='<tab>'
 let g:UltiSnipsJumpBackwardTrigger='<s-tab>'
-let g:UltiSnipsSnippetDirectories = ['~/.vim/UltiSnips']" >> ~/.vimrc
+let g:UltiSnipsSnippetDirectories = ['~/.vim/UltiSnips']
+EOF
+
+cp c.snippets cpp.snippets ~/.vim/bundle/vim-snippets/snippets/
+
 source $HOME/.bashrc
